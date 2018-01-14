45567034fe1e306c4311b8c44a7391398e55c3ab
Ke Wang <wangkekekexili@gmail.com>
2018-01-13 21:05:50 -0500

    play with custom flag

---
+++ go/flag/tokens.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"errors"
+	"flag"
+	"fmt"
+	"strings"
+)
+
+type tokens []string
+
+var _ flag.Value = &tokens{}
+
+func (t *tokens) String() string {
+	return strings.Join([]string(*t), ",")
+}
+
+func (t *tokens) Set(s string) error {
+	s = strings.TrimSpace(s)
+	if len(s) == 0 {
+		return errors.New("no tokens specified")
+	}
+	for _, token := range strings.Split(s, ",") {
+		*t = append(*t, token)
+	}
+	return nil
+}
+
+func main() {
+	var t tokens
+	flag.Var(&t, "tokens", "a list of comman separeted tokens")
+	flag.Parse()
+	fmt.Println(t)
+}
