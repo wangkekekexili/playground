0d934939d31521f5b91d5c761f7a105c5010e1b5
Ke Wang <wangkekekexili@gmail.com>
2018-01-15 20:44:58 -0500

    play with conversions.

--- go/reflect/string_header.go
+++ go/reflect/string_header.go
@@ -6,6 +6,8 @@ import (
 	"unsafe"
 )
 
+type str string
+
 func main() {
 	s1 := "hello world"
 	h1 := (*reflect.StringHeader)(unsafe.Pointer(&s1))
@@ -18,4 +20,8 @@ func main() {
 	s3 := s1
 	h3 := (*reflect.StringHeader)(unsafe.Pointer(&s3))
 	fmt.Printf("%X %X %d\n", &s3, h3.Data, h3.Len)
+
+	s4 := str("hello world")
+	h4 := (*reflect.StringHeader)(unsafe.Pointer(&s4))
+	fmt.Printf("%X %X %d\n", &s4, h4.Data, h4.Len)
 }
