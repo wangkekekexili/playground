75048f8a56aee316ac56ab750da63c9762b41a2b
Ke Wang <wangkekekexili@gmail.com>
2018-01-29 19:38:27 -0500

    float64 bits

--- go/number/float.go
+++ go/number/float.go
@@ -1,12 +1,37 @@
 package main
 
 import (
+	"bytes"
+	"encoding/binary"
 	"fmt"
+	"math"
 )
 
 func main() {
-	nums := []float32{0}
+	nans := []float64{
+		math.NaN(),
+		math.Copysign(math.NaN(), -1),
+		math.Float64frombits(0xFFFF000000000000),
+	}
+	for _, n := range nans {
+		fmt.Printf("%v %v\n", math.IsNaN(n), n)
+	}
+
+	nums := []float64{
+		0,
+		math.Copysign(0, -1),
+		math.NaN(),
+		math.Copysign(math.NaN(), -1),
+		math.Inf(1),
+		math.Inf(-1),
+		1,
+		2,
+		3,
+	}
+
 	for _, n := range nums {
-		fmt.Printf("%v: %b\n", n, n)
+		var b bytes.Buffer
+		binary.Write(&b, binary.BigEndian, n)
+		fmt.Printf("%08b %v\n", b.Bytes(), n)
 	}
 }
