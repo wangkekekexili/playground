bb075331ec50760d1a0a177fb7885092f95629a0
Ke Wang <wangkekekexili@gmail.com>
2018-05-12 22:35:15 -0400

    vgo

---
+++ go/vgo/go.mod
@@ -0,0 +1,3 @@
+module github.com/wangkekekexili/playground/go/vgo
+
+require rsc.io/quote v1.5.2
---
+++ go/vgo/main.go
@@ -0,0 +1,11 @@
+package main // import "github.com/wangkekekexili/playground/go/vgo"
+
+import (
+	"fmt"
+
+	"rsc.io/quote"
+)
+
+func main() {
+	fmt.Println(quote.Go())
+}
