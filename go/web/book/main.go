fd32eb3b57843b8f48ee350284d399ce0d1e62f3
Ke Wang <wangkekekexili@gmail.com>
2018-09-10 20:14:13 -0400

    Display result from classify2.

--- go/web/book/main.go
+++ go/web/book/main.go
@@ -3,10 +3,14 @@ package main
 import (
 	"database/sql"
 	"encoding/json"
+	"encoding/xml"
 	"html/template"
+	"io/ioutil"
 	"log"
 	"net/http"
+	"net/url"
 
+	"github.com/pkg/errors"
 	_ "rsc.io/sqlite"
 )
 
@@ -16,10 +20,14 @@ type Page struct {
 }
 
 type SearchResult struct {
-	Name   string `json:"name"`
-	Author string `json:"author"`
-	Year   string `json:"year"`
-	ID     string `json:"id"`
+	Title  string `json:"name" xml:"title,attr"`
+	Author string `json:"author" xml:"author,attr"`
+	Year   string `json:"year" xml:"hyr,attr"`
+	ID     string `json:"id" xml:"owi,attr"`
+}
+
+type ClassifySearchResult struct {
+	Results []SearchResult `xml:"works>work"`
 }
 
 func main() {
@@ -46,12 +54,29 @@ func main() {
 	})
 
 	http.HandleFunc("/search", func(w http.ResponseWriter, r *http.Request) {
-		results := []SearchResult{
-			{"Unlimited Memory", "Kevin", "2014", "B00I3QS1XQ"},
-			{"The Extraordinary Life of Sam Hell: A Novel", "Robert", "2018", "B07BNVZDM7"},
-		}
+		results, _ := search(r.FormValue("search"))
 		json.NewEncoder(w).Encode(results)
 	})
 
 	log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+func search(query string) ([]SearchResult, error) {
+	u := "http://classify.oclc.org/classify2/Classify?summary=true&title=" + url.QueryEscape(query)
+	resp, err := http.Get(u)
+	if err != nil {
+		return nil, errors.Wrap(err, "get from url")
+	}
+	defer resp.Body.Close()
+	b, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return nil, errors.Wrap(err, "read body")
+	}
+
+	r := &ClassifySearchResult{}
+	err = xml.Unmarshal(b, r)
+	if err != nil {
+		return nil, errors.Wrap(err, "unmarshal result")
+	}
+	return r.Results, nil
 }
